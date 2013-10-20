577d13d7eca89d7717e68795d911541c743662ef
Robert La Rocca <laroccrj@alfredstate.edu>
2013-10-20 17:18:09 -0400

    fixed issue with  in top.php

--- includes/top.php
+++ includes/top.php
@@ -1,36 +1,40 @@
 <?php
     include_once(ISSET($rootDir)? $rootDir.'back/user.php' : 'back/user.php');
     session_start();
+    
+    if(!ISSET($rootDir))
+        $rootDir = "";
 ?>
 <html>
 <head>
-<link rel="stylesheet" href="<?php if(ISSET($rootDir)) echo $rootDir; ?>style.css" type="text/css" />
-<script type="text/javascript" src="<?php if(ISSET($rootDir)) echo $rootDir; ?>javascript.js"></script>
+<link rel="stylesheet" href="<?php echo $rootDir; ?>style.css" type="text/css" />
+<script type="text/javascript" src="<?php echo $rootDir; ?>javascript.js"></script>
 </head>
 
 <body>
 <div id="container">
 
 <div id="header">
-<img src="<?php if(ISSET($rootDir)) echo $rootDir; ?>images/header.jpg" alt="Alfred State Logo" />
+<img src="<?php echo $rootDir; ?>images/header.jpg" alt="Alfred State Logo" />
 </div>
 
 <div id="nav">
 <ul>
-    <a href="<?php if(ISSET($rootDir)) echo $rootDir; ?>index.php"><li>Home</li></a>
+    <a href="<?php echo $rootDir; ?>index.php"><li>Home</li></a>
     <?php
+   
         if(ISSET($_SESSION['user'])) {
             switch($_SESSION['user']->info['type']) {
                 case(UserType::Student):
-                echo '<a href="'.$rootDir.'search.php"><li>Search Internships</li></a>';
-                break;
+                    echo '<a href="'.$rootDir.'search.php"><li>Search Internships</li></a>';
+                    break;
                 case(UserType::Admin):
-                echo '<a href="'.$rootDir.'admin/newAdmin.php"><li>Add a New Administrator</li></a>';
-                echo '<a href="'.$rootDir.'search.php"><li>Search Internships</li></a>';
-                break;
+                    echo '<a href="'.$rootDir.'admin/newAdmin.php"><li>Add a New Administrator</li></a>';
+                    echo '<a href="'.$rootDir.'search.php"><li>Search Internships</li></a>';
+                    break;
                 case(UserType::Company):
-                echo '<a href="'.$rootDir.'company/viewInternship.php"><li>Check your Internships</li></a>';
-                break;  
+                    echo '<a href="'.$rootDir.'company/viewInternship.php"><li>Check your Internships</li></a>';
+                    break;  
             }
             echo '<a href="'.$rootDir.'logout.php"><li>Logout</li></a>';
         }
