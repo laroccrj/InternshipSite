27a4cfa8fafc19ae46f284b260510a999c8d1c92
Adam Smith <smithaj@alfredstate.edu>
2013-11-07 13:52:59 -0500

    user.php line 125 can't do the [1], moved to ln128 for check

--- back/user.php
+++ back/user.php
@@ -122,10 +122,10 @@ class Student extends User
         if(!filter_var($email, FILTER_VALIDATE_EMAIL))
             throw new InvalidEmailException();
             
-        $domain = explode("@", $email)[1];
+        $domain = explode("@", $email);
         var_dump($domain);
         
-        if($domain != "alfredstate.edu")
+        if($domain[1] != "alfredstate.edu")
             throw new InvalidEmailException();
         
         $conn = User::getConnection();
