9dd26418448374658cd18fa01ad076afcf8e1e5a
Brandon <dioguabs@alfredstate.edu>
2013-11-09 19:52:41 -0500

    Added a topcomapny include so companies are different than the other 2 #21

---
+++ includes/topCompany.php
@@ -0,0 +1,23 @@
+<?php
+    include_once(ISSET($rootDir)? $rootDir.'back/user.php' : 'back/user.php');
+    session_start();
+?>
+<html>
+<head>
+<link rel="stylesheet" href="<?php if(ISSET($rootDir)) echo $rootDir; ?>style.css" type="text/css" />
+<script type="text/javascript" src="<?php if(ISSET($rootDir)) echo $rootDir; ?>javascript.js"></script>
+</head>
+
+<body>
+<div id="container">
+
+<div id="header">
+<img src="<?php if(ISSET($rootDir)) echo $rootDir; ?>images/header.jpg" alt="Alfred State Logo" />
+</div>
+
+<div id="nav">
+<ul>
+    <a href="index.php"><li>Home</li></a>
+    <a href="viewInternship.php"><li>Check your Internships</li></a>
+</ul>
+</div>
