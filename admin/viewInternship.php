86e3d1b11377f6164b59b0a05ec06cb8a4856e9f
Brandon <dioguabs@alfredstate.edu>
2013-11-19 09:00:43 -0500

    Started adding the search function for admin and student #53

---
+++ admin/viewInternship.php
@@ -0,0 +1,22 @@
+<?php
+$rootDir = "../";
+include_once('../includes/top.php');
+?>
+<?php    
+    User::checkLogin(UserType::Admin, $rootDir);
+    $user = $_SESSION["user"];
+    
+    require_once('../back/internship.php');
+    $internship = new Internship($_GET["id"]);
+?>
+<div id="content">
+<div style="margin: auto;">
+<h2 style="text-align: center;"><?php echo $internship->info["title"]; ?></h2>
+<?php
+    include_once("../views/internshipPost.php");
+?>
+</div>
+<br />
+</div>
+
+<?php include_once('../includes/bottom.php'); ?>
--- search.php
+++ search.php
@@ -1,23 +1,31 @@
 <?php include_once('includes/top.php'); ?>
-
+<?php
+    require_once('back/internship.php');
+$title = "";
+$year = "";
+$semester = "";
+$location = "";
+?>
 <div id="content">
 	<h3 id="breadcrumb">Home > Search Internships</h3>
 <form action="" method="post">
 <table cellspacing="10" cellpadding="1">
 	<tr>
-		<td>Keywords:</td>
-		<td><input type="text" name="keywords" value="" /></td>
+		<td>Title:</td>
+		<td><input type="text" name="title" value="" /></td>
 	</tr>
-	<tr>
+<!--	<tr>
 		<td>Location:</td>
 		<td><input type="text" name="location" value="" /></td>
-	</tr>
+	</tr> -->
 	<tr>
 		<td>Semester:</td>
-		<td><select name="semester" style="width: 75px;">
-				<option value="Fall">Fall</option>
-				<option value="Spring">Spring</option>				
-			</select></td>
+			<td><select name="semester" style="width: 75px;">
+					<option value="fall">fall</option>
+					<option value="spring">spring</option>
+					<option value="summer">summer</option>	
+					<option value="All">All-Round</option>
+				</select></td>
 	</tr>
 	<tr>
 			<td>Year:</td>
@@ -36,7 +44,47 @@
 
 
 <div id="searchResults">
+<?php
+$title = $_POST['title'];
+$year = $_POST['year'];
+$semester = $_POST['semester'];
 
+$internships = Internship::getInternships(array("title" => $title));
+	if($internships->count() >= 1)
+    {	
+	?>
+	<table class ="data">
+		<th>Title</th>
+	<?php
+		foreach ($internships as $internship)
+		{
+			if(ISSET($_SESSION['user'])) 
+			{
+				switch($_SESSION['user']->info['type']) 
+				{
+					case(UserType::Student):
+				?>
+					<tr>
+						<td><a href="../student/viewInternship.php?id=<?php echo $internship['_id']; ?>"><?php echo $internship["title"]; ?></a></td>
+					</tr>
+				<?php
+					break;
+					case(UserType::Admin):
+				?>
+					<tr>
+						<td><a href="../admin/viewInternship.php?id=<?php echo $internship['_id']; ?>"><?php echo $internship["title"]; ?></a></td>
+					</tr>
+				<?php
+					break;
+				}
+				?>
+
+	</table>
+	<?php
+			}
+		}
+	}
+	?>
 </div>
 <br />
 </div>
